b23d9263ea0950165c91fd849b54b5ce4c3c0d48
Tigeressa <63375633+Tigeressa@users.noreply.github.com>
2021-03-15 10:54:19 +0000

    fixed toogle menu on links so that it retracts on a link click.

--- script/nav.js
+++ script/nav.js
@@ -3,21 +3,27 @@ const navSlide = ()=> {
 	const nav = document.querySelector('.nav-links');
 	const navLinks = document.querySelectorAll('.nav-links li')
 
-	hamburger.addEventListener('click', () => {
+	const toggleMenu = () => {
 		// Toggle nav
 		nav.classList.toggle('nav-active');
 
 	// Animate navlinks
 		navLinks.forEach((link, index) => {
 			if (link.style.animation) {
-			link.style.animation = ''
+				link.style.animation = ''
 			} else {
-			link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 0.5}s`;
+				link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 0.5}s`;
 			}
 		});
 		// Hamburger animation
 		hamburger.classList.toggle('toggle');
-	});
+	}
+
+	hamburger.addEventListener('click', toggleMenu);
+	navLinks.forEach((link) => {
+		link.addEventListener('click', toggleMenu);
+	}); 
 }
+
 //envoke the function 
 navSlide(); 
